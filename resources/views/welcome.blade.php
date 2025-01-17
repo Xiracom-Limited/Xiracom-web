c438399993aa54fbbaa615f5c3f0f7a9baedf95b
Proffesuer <levischisira0@gmail.com>
2025-01-17 22:12:09 +0300

    included the nav bar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,7 @@
+
+@include("navigation-bar")
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,28 +12,6 @@
 </head>
 <body>
 
-    <!-- Navbar starts here-->
-
-  <div class="header-wrapper">
-    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/a5016053130a5fc70271270a2305caafee1ce05308b14e2bf7842f252a184fec?placeholderIfAbsent=true&apiKey=b253cd62eabd450abd701eab9cc667e8" alt="Company Logo" class="logo" />
-    <nav class="nav-container">
-      <div class="menu-wrapper">
-        <a href="#" class="home-link">
-          <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/d756e8c8126240957051134d6a08d14dc7246ef6a0fe66a1990f300025f683bd?placeholderIfAbsent=true&apiKey=b253cd62eabd450abd701eab9cc667e8" alt="Home Icon" class="home-icon" />
-        </a>
-        <a href="#" class="nav-link">About us</a>
-        <a href="#" class="nav-link">Projects</a>
-        <a href="#" class="nav-link">Our Services</a>
-        <a href="#" class="nav-link">Sections</a>
-        <a href="#" class="nav-link">Contact us</a>
-      </div>
-      <div class="auth-container">
-        <div class="signup-wrapper">
-          <button class="get-started-btn">Get Started</button>
-        </div>
-      </div>
-    </nav>
-  </div>
 
    <!-- Hero section starts here-->
 
