50d3c979494703db3c4605ceb156b3fb78754c6d
Proffesuer <levischisira0@gmail.com>
2025-01-17 21:59:34 +0300

    finished the navigation bar design

--- resources/views/navigation-bar.blade.php
+++ resources/views/navigation-bar.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="style/navigation-bar.css">
+<link rel="stylesheet" href="style/navbar.css">
   @vite(['resources/sass/app.scss','resources/js/app.js'])
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -12,79 +12,82 @@
 <body>
   
 <!--Navigation bar starts here-->
-<nav class="container navbar navbar-expand-lg navbar-light bg-white" id="nav-bar">
-  <a class="navbar-brand" href="#"><img src="images/logo.svg" width="60%"></a>
+<nav class="container navbar navbar-expand-lg" id="nav-bar">
+  <a class="navbar-brand" href="#"><img src="images/logo.svg" width="80%"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
+  <div class="collapse navbar-collapse nav-bar-bg" id="navbarNavDropdown">
+    <ul class="navbar-nav  navbar-bg" style="padding-left:3%;padding-right:2%;">
+     
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" style="font-weight:bolder;color:white;"  href="#">Home</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle"style="font-weight:bolder;color:white;" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           About Us
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Who We Are?</a>
-          <a class="dropdown-item" href="#">Our Team</a>
-          <a class="dropdown-item" href="#">Jobs & Internships</a>
-          <a class="dropdown-item" href="#">Our Projects</a>
-          <a class="dropdown-item" href="#">Our Clients</a>
-          <a class="dropdown-item" href="#">Blog</a>
+        <div class="dropdown-menu" style="background-color:whitesmoke;"aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Who We Are?</a>
+          <a class="dropdown-item" style="font-weight:bolder;" href="#">Our Team</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Jobs & Internships</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Our Projects</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Our Clients</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Blog</a>
         </div>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" style="font-weight:bolder;color:white;" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Software Dev
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">UI/UX Design</a>
-          <a class="dropdown-item" href="#">App Development</a>
-          <a class="dropdown-item" href="#">Web Design & Dev</a>
-          <a class="dropdown-item" href="#">APIs Intergrations</a>
-          <a class="dropdown-item" href="#">ERP&CMS Systems</a>
-          <a class="dropdown-item" href="#">Support</a>
-          <a class="dropdown-item" href="#">Domain & Hosting</a>
+        <div class="dropdown-menu bg-color-drop" style="background-color:whitesmoke;"aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">UI/UX Design</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">App Development</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Web Design & Dev</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">APIs Intergrations</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">ERP&CMS Systems</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Support</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Domain & Hosting</a>
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Bulk SMS</a>
+        <a class="nav-link"style="font-weight:bolder;color:white;"  href="#">Bulk SMS</a>
       </li>
      
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" style="font-weight:bolder;color:white;"  href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Our Brands
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">DarasaLink</a>
-          <a class="dropdown-item" href="#">Examparlour</a>
-          <a class="dropdown-item" href="#">Onfod SMS</a>
-          <a class="dropdown-item" href="#">Xiracom BTC</a>
+        <div class="dropdown-menu"style="background-color:whitesmoke;"  aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" style="font-weight:bolder;" href="#">DarasaLink</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Examparlour</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Onfod SMS</a>
+          <a class="dropdown-item"style="font-weight:bolder;" href="#">Xiracom BTC</a>
         </div>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" style="font-weight:bolder;color:white;" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Other Services
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Tech Consultancy</a>
-          <a class="dropdown-item" href="#">Shop</a>
-          <a class="dropdown-item" href="#">Skiza Tunes</a>
-          <a class="dropdown-item" href="#">Cybersecurity</a>
-          <a class="dropdown-item" href="#">Digital Marketing</a>
-          <a class="dropdown-item" href="#">Trainings & Bootcamps</a>
+        <div class="dropdown-menu bg-color-drop" style="background-color:whitesmoke;"aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Tech Consultancy</a>
+          <a class="dropdown-item"style="font-weight:bolder;" href="#">Shop</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Skiza Tunes</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Cybersecurity</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Digital Marketing</a>
+          <a class="dropdown-item" style="font-weight:bolder;"href="#">Trainings & Bootcamps</a>
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Contact Us</a>
+        <a class="nav-link" style="font-weight:bolder;color:white;" href="#">Contact Us</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">GET STARTED</a>
+      <li class="login">
+        <a class="nav-link"style="font-weight:bolder;color:white;border-style:solid;border-color:green;"  href="#">Get Started</a>
       </li>
-    </ul>
+    </ul> 
+   
   </div>
+ 
 </nav>
 <!--navigation bar ends here -->
 
