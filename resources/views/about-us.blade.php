80c3c6b4d99982550969abe4e502e67dc17f7f58
Proffesuer <levischisira0@gmail.com>
2025-01-28 16:13:50 +0300

    created a responsive about me section

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -3,8 +3,6 @@
 
 
 
-
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,9 +14,8 @@
     <!--heroes section starts here-->
 <div class="heroes-section">
 
-<img src="images/about.png" width="100%">
+<img src="images/about.png" width="100%" height="60%">
 
-<h class="about-txt">ABOUT US</h>
 </div>
 <!--heroes section ends here-->
 <!--Who we are section-->
@@ -26,31 +23,60 @@
 
 <div class="row">
 <div class="col-md-5 col-sm-6 col-lg-5 about-section-one ">
- <div class="bg-icon">
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
-hello world my name is chiira levis and this is my left section for productions
+ <div class="bg-icon txt-about">
+<h>ABOUT US</h>
+<P>Lorem ipsum dolor sit amet, 
+    consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, 
+    pulvinar dapibus leo.
+    Lorem ipsum dolor sit amet, 
+    consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, 
+    pulvinar dapibus leo.</P>
 </div>
 </div>
 <div class="col-md-7 col-sm-6 col-lg-7 about-section-two">
-hello
+<h3>WHO WE ARE</h3>
+<h1>WE ARE PERFECT TEAM FOR TECH DESIGN</h1>
+<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+    doloremque laudantium, totam rem aperiam, 
+    eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+Nemo enim ipsam voluptatem quia voluptas sit aspernatur 
+aut odit aut fugit, sed quia consequuntur magni dolores eos 
+qui ratione voluptatem sequi nesciunt.</p>
+<div class="row ms-0" >
+    <div class="col-md-6">
+<ul class="list">
+    <li><img src="images/tick.svg">Flexible Time</li>
+    <li><img src="images/tick.svg">Flexible Time</li>
+    <li><img src="images/tick.svg">Flexible Time</li>
+    
+</ul>
+    </div>
+    <div class="col-md-6">
+    <ul class="list">
+    <li><img src="images/tick.svg">Flexible Time</li>
+    <li><img src="images/tick.svg">Flexible Time</li>
+    <li><img src="images/tick.svg">Flexible Time</li>
+    
+</ul>
+</div>
+
+
+</div>
+<div class="row ms-5">
+    <div class="col-md-4">
+<h>15Y</h>
+<p>Experience</p>
+    </div>
+    <div class="col-md-4">
+<h>25+</h>
+<p>Best Team</p>
+</div>
+<div class="col-md-4">
+<h>500+</h>
+<p>Total Client</p>
+</div>
+
+</div>
 </div>
 
 
@@ -60,6 +86,44 @@ hello
 
 </div>
 <!--who we are section ends here-->
+<!-- Our team Section starts here-->
+<div class="our-team container">
+<div>
+    OUR TEAM
+</div>
+<div class="row">
+<div class="col-md-4 col-sm-4 col-lg-4 team-card">
+<div class="upper-section-1">
+
+</div>
+<div class="lower-section">
+
+</div>
+</div>
+<div class="col-md-4 col-sm-4 col-lg-4 team-card">
+<div class="upper-section-1">
+
+</div>
+<div class="lower-section">
+
+</div>
+
+</div>
+<div class="col-md-4 col-sm-4 col-lg-4 team-card">
+<div class="upper-section-1">
+
+</div>
+<div class="lower-section">
+
+</div>
+</div>
+
+</div>
+
+
+</div>
+<!--our team section ends here-->
+
 
 <!--body ends here-->
 
