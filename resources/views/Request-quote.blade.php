5d630b84c72f70f4544a33e7d0144a1ae3cd79d3
James Shadrack Wafula <122097201+James-Shadrack-Wafula@users.noreply.github.com>
2025-01-29 09:16:21 +0300

    style request page

--- resources/views/Request-quote.blade.php
+++ resources/views/Request-quote.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Request Quote</title>
     
-    <!-- Bootstrap CSS -->
+    <!-- Bootstrap CSS --> 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     
     <!-- Custom CSS -->
