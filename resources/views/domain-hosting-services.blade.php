f9381ec58f9f6bc2f95683a036acff9346c50f69
James Shadrack Wafula <122097201+James-Shadrack-Wafula@users.noreply.github.com>
2025-01-29 21:17:27 +0300

    style blog and web host page

---
+++ resources/views/domain-hosting-services.blade.php
@@ -0,0 +1,391 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Hosting</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+
+    <style>
+        .banner-text h1 {
+            /* Discover Top-Notch Hosting and Domain Solutions */
+
+
+
+            /* font-family: 'Poppins'; */
+            font-style: normal;
+            font-weight: 700;
+            font-size: 48px;
+            line-height: 109.1%;
+            /* or 52px */
+            display: flex;
+            align-items: center;
+            text-align: center;
+            letter-spacing: -0.04em;
+
+            color: #000000;
+
+
+        }
+
+        .banner-text p {
+            /* We are the best in the business. Get started with us today */
+
+            /* position: absolute;
+width: 683px;
+height: 68px;
+left: 31px;
+top: 397px; */
+
+            /* font-family: 'Poppins'; */
+            font-style: normal;
+            font-weight: 400;
+            font-size: 36px;
+            line-height: 109.1%;
+            /* or 39px */
+            display: flex;
+            align-items: center;
+            text-align: center;
+            letter-spacing: -0.04em;
+
+            color: #000000;
+
+
+        }
+
+        .c-btn {
+            background: #EDEDF5;
+            box-sizing: border-box;
+
+            /* Auto layout */
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            padding: 10px 28px;
+
+            background: #399C23;
+            border: 1.5px solid #102038;
+            border-radius: 72.0899px;
+            /* font-family: 'Poppins'; */
+            font-style: normal;
+            font-weight: 700;
+            font-size: 20px;
+            line-height: 22px;
+            /* or 112% */
+            display: flex;
+            align-items: center;
+            text-align: center;
+
+            color: #FFFFFF;
+
+
+        }
+
+        .c-card-cont {
+            /* margin-left: 20px;
+            margin-right: 20px; */
+            width: 80%;
+            align-items: center;
+            justify-content: center !important;
+
+        }
+
+
+        .card {
+            /* Rectangle 2003 */
+
+            /* position: absolute;
+width: 350px;
+height: 413px;
+left: 99px;
+top: 852px; */
+            width: 85%;
+            background: #FFFFFF;
+            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+            border-radius: 30px;
+            height: 313px;
+
+
+
+            /* Rectangle 2006 */
+
+
+
+            /* background: linear-gradient(180deg, #FFFFFF 12.5%, #7A87FB 100%); */
+
+
+
+
+        }
+
+        .c-card-b .card {
+            background: linear-gradient(180deg, #FFFFFF 12.5%, #7A87FB 100%);
+            border: none;
+            width: 270px;
+
+        }
+        .c-card-b{
+            
+        }
+
+
+        .c-bottom {
+            width: 80%;
+            /* text-align: center; */
+            align-items: center !important;
+            margin: auto;
+            padding: 20px;
+            height: 125px;
+            /* background-color: #999999; */
+            border-radius: 18px;
+
+        }
+
+        .c-bottom h2 {
+            font-size: 1.8rem;
+            font-weight: bold;
+        }
+
+        .c-btm-button {
+            /* padding: 10px; */
+            /* width: 100px; */
+            /* height: 40px; */
+            margin: auto;
+            font-size: 16px;
+            background-color: #28a745;
+
+            border: 1.5px solid #102038;
+            border-radius: 72.0899px;
+
+        }
+
+        .c-btm-section {
+            align-items: center;
+            background: linear-gradient(180deg, #FFFFFF 12.5%, #D9D9D9 100%);
+
+
+        }
+    </style>
+    <section class="bunner bg-light">
+        <div class="container-fluid">
+
+            <div class="row banner">
+                <!-- Left Image -->
+
+
+                <div class="col-md-6 d-flex align-items-center px-md-5">
+                    <div class="banner-text">
+                        <h1 class="fs-sm-5">Discover Top-Notch Hosting and Domain Solutions</h1>
+                        <p>We are the best in the business. Get started with us today
+                        </p>
+
+                        <button class="c-btn ">Get started</button>
+                    </div>
+                </div>
+                <!-- Right Text -->
+                <div class="col-md-6 p-0 d-flex align-items-center">
+                    <img src="images/network-servers-isolated_98292-6502 1.png" alt="Banner Image">
+                </div>
+
+            </div>
+        </div>
+
+        <div class="container-fluid mt-5  w-100 d-flex justify-content-center">
+            <h3 class="fw-bolder">Why Choose us</h3>
+        </div>
+        <div class="container-fluid mt-5 c-card-cont">
+            <div class="row row-cols-1 row-cols-md-3 g-2">
+                <div class="col d-flex justify-content-center">
+                    <div class="card mx-auto p-3">
+                        <div class="text-center mb-3">
+                            <img src="images/Security SSL.svg" alt="CSV Icon" width="50">
+                        </div>
+
+                        <h5 class="card-title p-2 fw-bolder text-center ">Free SSL Security</h5>
+                        <div class="card-body ">
+                            <div class="d-flex align-items-center">
+                                <!-- CSV Icon -->
+                                <img src="images/Check Mark.svg" alt="CSV Icon" width="30" class="me-2">
+                                <!-- Text -->
+                                <p class="card-text mb-0">Secure data</p>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <!-- CSV Icon -->
+                                <img src="images/Check Mark.svg" alt="CSV Icon" width="30" class="me-2">
+                                <!-- Text -->
+                                <p class="card-text mb-0">Build Trust</p>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <!-- CSV Icon -->
+                                <img src="images/Check Mark.svg" alt="CSV Icon" width="30" class="me-2">
+                                <!-- Text -->
+                                <p class="card-text mb-0">Boost SEO</p>
+                            </div>
+
+
+
+                        </div>
+
+
+
+                    </div>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <div class="card mx-auto p-3">
+                    <div class="text-center mb-3">
+                            <img src="images/Security SSL.svg" alt="CSV Icon" width="50">
+                        </div>
+
+                        <h5 class="card-title p-2 fw-bolder text-center ">Free SSL Security</h5>
+                        <div class="card-body ">
+                            <div class="d-flex align-items-center">
+                                <!-- CSV Icon -->
+                                <img src="images/Check Mark.svg" alt="CSV Icon" width="30" class="me-2">
+                                <!-- Text -->
+                                <p class="card-text mb-0">Secure data</p>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <!-- CSV Icon -->
+                                <img src="images/Check Mark.svg" alt="CSV Icon" width="30" class="me-2">
+                                <!-- Text -->
+                                <p class="card-text mb-0">Build Trust</p>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <!-- CSV Icon -->
+                                <img src="images/Check Mark.svg" alt="CSV Icon" width="30" class="me-2">
+                                <!-- Text -->
+                                <p class="card-text mb-0">Boost SEO</p>
+                            </div>
+
+
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <div class="card mx-auto p-3">
+                    <div class="text-center mb-3">
+                            <img src="images/Security SSL.svg" alt="CSV Icon" width="50">
+                        </div>
+
+                        <h5 class="card-title p-2 fw-bolder text-center ">Free SSL Security</h5>
+                        <div class="card-body ">
+                            <div class="d-flex align-items-center">
+                                <!-- CSV Icon -->
+                                <img src="images/Check Mark.svg" alt="CSV Icon" width="30" class="me-2">
+                                <!-- Text -->
+                                <p class="card-text mb-0">Secure data</p>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <!-- CSV Icon -->
+                                <img src="images/Check Mark.svg" alt="CSV Icon" width="30" class="me-2">
+                                <!-- Text -->
+                                <p class="card-text mb-0">Build Trust</p>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <!-- CSV Icon -->
+                                <img src="images/Check Mark.svg" alt="CSV Icon" width="30" class="me-2">
+                                <!-- Text -->
+                                <p class="card-text mb-0">Boost SEO</p>
+                            </div>
+
+
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="container-fluid mt-5  w-100 d-flex justify-content-center">
+            <h3 class="fw-bolder">Web Hosting Plans</h3>
+        </div>
+
+
+        <div class="container-fluid mt-5 d-flex justify-content-center c-card-cont c-card-b">
+            <div class="row row-cols-1 row-cols-md-3 g-2 ">
+                <div class="col d-flex justify-content-center">
+                    <div class="card mx-auto p-3">
+                        <h5 class="card-title p-2 fw-bolder text-center">Regular</h5>
+                        <h6 class="card-subtitle mb-2 text-muted text-center">KES 5,000/yre</h6>
+                        <div class="card-body">
+                            <ul>
+                                <li>30GB SSD disk space</li>
+                                <li>Host up to 3 domains</li>
+                                <li>Unlimited FTP </li>
+                                <li>Accounts</li>
+                                <li>Shared IP address</li>
+                                <li>Unlimited databases</li>
+                            </ul>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <div class="card mx-auto p-3">
+                        <!-- <h5 class="card-title p-2 fw-bolder">The Future of AI</h5> -->
+                        <h5 class="card-title p-2 fw-bolder text-center">Starlight</h5>
+                        <h6 class="card-subtitle mb-2 text-muted text-center">KES 10,000 /yr</h6>
+                        <div class="card-body">
+                            <ul>
+                                <li>50GB SSD disk space</li>
+                                <li>Host up to 30 domains</li>
+                                <li>Unlimited databases </li>
+                                <li>Unlimited FTP </li>
+                                <li>Accounts</li>
+                                <li>Shared IP address</li>
+                            </ul>
+                            
+                        </div>
+
+                        
+                    </div>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <div class="card mx-auto p-3">
+                        <!-- <h5 class="card-title p-2 fw-bolder">The Future of AI</h5> -->
+                        <h5 class="card-title p-2 fw-bolder text-center">Premium</h5>
+                        <h6 class="card-subtitle mb-2 text-muted text-center">KES 20,000/yr</h6>
+                        <div class="card-body">
+                            <ul>
+                                <li>30GB SSD disk space</li>
+                                <li>Host up to 3 domains</li>
+                                <li>Unlimited FTP </li>
+                                <li>Accounts</li>
+                                <li>Shared IP address</li>
+                                <li>Unlimited databases</li>
+                            </ul>
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-light py-5 c-btm-section">
+
+        <div class=" row container text-center c-bottom bg-light">
+            <div class="row ">
+                <h2 class="col-sm-10 fs-4 ">Get a Cyber Security Assessment today...</h2>
+                <a href="#" class="btn btn-primary btn-lg col-sm-2 c-btm-button">Get Started</a>
+
+            </div>
+
+        </div>
+    </section>
+</body>
+
+</html>
