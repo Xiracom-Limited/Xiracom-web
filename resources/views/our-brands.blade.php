d50114d471d387d5fa8e60889b5fd08ce6e79f5c
Proffesuer <levischisira0@gmail.com>
2025-02-02 02:35:49 +0300

    creted a new file

---
+++ resources/views/our-brands.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Brands</title>
+    <link rel="stylesheet" href="style/brands.css">
+    @vite(['resources/sass/app.scss','resources/js/app.js'])
+</head>
+<body>
+<!--Heroes section starts here-->
+<div class="heroes-section-brands">
+    <h1 class="heroes-section-head1">
+        Our Brands
+    </h1>
+    <img src="images/port3.png" class="image-heroes">
+
+
+
+</div>
+
+
+
+<!--Heroes section ends here-->
+
+
+
+    
+</body>
+</html>
