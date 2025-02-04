48f4b44e34ecdc0e1f0776609d9ae6751ae6c84c
Proffesuer <levischisira0@gmail.com>
2025-02-04 18:52:15 +0300

    changed background color

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,178 @@
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="fonts/icomoon/style.css">
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<!-- Style CSS -->
+<link rel="stylesheet" href="style/footer.css">
+</head>
+
+<body>
+  <style>
+    .footer {
+      background-color: #1a1a1a;
+      color: #fff;
+      padding: 50px 0;
+      font-size: 15px;
+    }
+
+    .footer h5 {
+      font-weight: 700;
+      margin-bottom: 20px;
+      color: #f8f9fa;
+    }
+
+    .footer a {
+      color: #bbb;
+      text-decoration: none;
+    }
+
+    .footer a:hover {
+      color: #fff;
+    }
+
+    .social-icons a {
+      margin-right: 15px;
+      font-size: 24px;
+      transition: 0.3s;
+    }
+
+    .social-icons a:hover {
+      color: #007bff;
+    }
+
+    .newsletter input {
+      width: 100%;
+      padding: 12px;
+      border-radius: 25px;
+      border: 1px solid #444;
+      background: #2c2c2c;
+      color: #fff;
+      outline: none;
+    }
+
+    .newsletter button {
+      margin-top: 10px;
+      width: 100%;
+      border-radius: 25px;
+      padding: 12px;
+      background: #007bff;
+      border: none;
+      color: white;
+      font-weight: 600;
+      transition: 0.3s;
+    }
+
+    .newsletter button:hover {
+      background: #0056b3;
+    }
+
+    .copyright {
+      background-color: #111;
+      padding: 15px 0;
+      text-align: center;
+      font-size: 14px;
+      margin-top: 30px;
+    }
+  </style>
+
+
+<footer class="footer bg-dark text-light py-5 ">
+  <div class="container">
+    <div class="row">
+      <!-- Contact Section -->
+      <div class="col-md-3">
+  <h5 class="fw-bolder">Contact Us</h5>
+  <ul class="list-unstyled">
+    <li><i class="fab fa-whatsapp text-success"></i> +48729628367</li>
+    <li><i class="fab fa-whatsapp text-success"></i> +48579205541</li>
+    <li><i class="fas fa-phone text-primary"></i> +254704907555</li>
+    <li><i class="fas fa-envelope text-warning"></i> info@xiracom.co.ke</li>
+  </ul>
+</div>
+
+
+      <!-- Services Section -->
+      <div class="col-md-3">
+        <h5>Our Services</h5>
+        <ul class="list-unstyled">
+          <li><a href="#">UI/UX Design</a></li>
+          <li><a href="#">App Development</a></li>
+          <li><a href="#">Web Design & Development</a></li>
+          <li><a href="#">APIs Integration</a></li>
+          <li><a href="#">Domain & Hosting</a></li>
+        </ul>
+      </div>
+
+      <!-- Brand Section -->
+      <div class="col-md-3">
+        <h5>Our Brand</h5>
+        <ul class="list-unstyled">
+          <li><a href="#">DarasaLink</a></li>
+          <li><a href="#">Examparlour</a></li>
+          <li><a href="#">Onfod SMS</a></li>
+          <li><a href="#">Xiracom BTC</a></li>
+          <li><a href="#">Skiza Tunes</a></li>
+        </ul>
+      </div>
+
+      <!-- Newsletter Section -->
+      <div class="col-md-3">
+        <h5>Newsletter</h5>
+        <p>Subscribe to our newsletter for updates.</p>
+        <div class="newsletter ">
+          <input type="email" class="form-control me-2 bg-white" placeholder="Enter your email">
+          <button class="btn btn-primary" style="background-color: #399C23">Subscribe</button>
+        </div>
+      </div>
+    </div>
+
+    <!-- Contact & Social Media -->
+    <div class="row mt-4 ">
+      <div class="col">
+        <h5>Our Socials</h5>
+        <div class="social-icons">
+          <a href="#" class="text-light mx-2"><i class="fab fa-twitter fs-4"></i></a>
+          <a href="#" class="text-light mx-2"><i class="fab fa-youtube fs-4"></i></a>
+          <a href="#" class="text-light mx-2"><i class="fab fa-linkedin fs-4"></i></a>
+          <a href="#" class="text-light mx-2"><i class="fab fa-facebook fs-4"></i></a>
+          <a href="#" class="text-light mx-2"><i class="fab fa-instagram fs-4"></i></a>
+          <a href="#" class="text-light mx-2"><i class="fab fa-whatsapp fs-4"></i></a>
+        </div>
+      </div>
+    </div>
+<hr>
+    <!-- Google Map -->
+    <div class="row mt-4">
+      <div class="col">
+        <h5 class="text-center">Our Location</h5>
+        <p class="text-center">We are located At Eldoret, Kenya</p>
+        <div class="map-container">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15955.044065617223!2d39.5025871!3d-3.5447325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM8KwMzInMzYuOCJTIDM5wrAzMCcwOS4zIkU!5e0!3m2!1sen!2ske!4v1712345678901"
+            width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy">
+          </iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Copyright Section -->
+  <div class="copyright mt-4 text-center">
+    <p class="m-0">&copy; 2024 Safe Pesa. All Rights Reserved.</p>
+  </div>
+</footer>
+
+<!-- Bootstrap & FontAwesome (Ensure these are included) -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+
+  <!-- Bootstrap Icons (Make sure Bootstrap Icons CDN is included) -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
