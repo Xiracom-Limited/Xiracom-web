c1aa78ccf97268329aa9e277b30752ba6c9963e4
Proffesuer <levischisira0@gmail.com>
2025-02-06 13:57:13 +0300

    responsive items

--- resources/views/apiintergrations.blade.php
+++ resources/views/apiintergrations.blade.php
@@ -13,8 +13,8 @@
 
     <header class="hero">
         <div class="container">
-        <img src="images\api intergrations1.jpeg" alt="api intergrations" class="hero-image">
-            <h1>Payment <span class="highlight">Integration</span></h1>
+      
+            <h1>REQUEST A <span class="highlight">QUOTE FROM US</span></h1>
             <p>Mpesa is one of the best ways people make transactions in Kenya. 
                You can now integrate Mpesa into your online business for faster transactions.</p>
         </div>
@@ -27,41 +27,43 @@
 
             <form>
                 <div class="form-grid">
-                    <input type="text" placeholder="Full Name">
+                    <input type="text" required placeholder="Full Name">
                     <input type="email" placeholder="Email Address">
-                    <input type="tel" placeholder="Mobile Number">
+                    <input type="tel" required placeholder="Mobile Number">
                     <input type="text" placeholder="Business Name">
                 </div>
 
                 <h3>Select Other Services, Solutions or Packages</h3>
-                <div class="services">
-                    <div class="service-group">
-                        <label><input type="checkbox"> Service Inquiry</label>
-                        <label><input type="checkbox"> Website Design</label>
-                        <label><input type="checkbox"> Website Maintenance</label>
-                        <label><input type="checkbox"> Software & Mobile App Development</label>
-                        <label><input type="checkbox"> Academic Script Writing</label>
-                        <label><input type="checkbox"> Ecommerce Website</label>
-                        <label><input type="checkbox"> Payment Integration - M-PESA & Other Payments</label>
+                <div class="row">
+                    <div class="col-md-6">
+                    <input type="checkbox"> <label class="ml-1">Service Inquiry</label><br>
+                    <input type="checkbox"><label class="ml-1"> Website Design</label><br>
+                    <input type="checkbox">  <label class="ml-1">Website Maintenance</label><br>
+                    <input type="checkbox"><label class="ml-1"> Software & Mobile App Development</label><br>
+                    <input type="checkbox">   <label class="ml-1">Academic Script Writing</label><br>
+                    <input type="checkbox"> <label class="ml-1"> Ecommerce Website</label><br>
+                    <input type="checkbox"><label class="ml-1"> Payment Integration - M-PESA & Other Payments</label><br>
                     </div>
-                    <div class="service-group">
-                        <label><input type="checkbox"> Domain, Hosting, SSL & Gsuite</label>
-                        <label><input type="checkbox"> Bulk SMS & Bulk WhatsApp</label>
-                        <label><input type="checkbox"> Digital Marketing Training</label>
-                        <label><input type="checkbox"> SEO, Social Media, Paid Ads, Emails</label>
-                        <label><input type="checkbox"> Graphic Design, Logo, Printing & Branding</label>
-                        <label><input type="checkbox"> Photography & Video Production</label>
+                    <div class="col-md-6">
+                    <input type="checkbox"><label class="ml-1"> Domain, Hosting, SSL & Gsuite</label><br>
+                    <input type="checkbox"><label class="ml-1"> Bulk SMS & Bulk WhatsApp</label><br>
+                        <input type="checkbox"> <label class="ml-1">Digital Marketing Training</label><br>
+                        <input type="checkbox">  <label class="ml-1"> SEO, Social Media, Paid Ads, Emails</label><br>
+                        <input type="checkbox">  <label class="ml-1">Graphic Design, Logo, Printing & Branding</label><br>
+                        <input type="checkbox"> <label class="ml-1"> Photography & Video Production</label><br>
                     </div>
                 </div>
+                <br>
 
-                <input type="text" placeholder="How did you hear about us?" class="full-width">
+                <input type="text" placeholder="How did you hear about us?" class="full-width">   <br><br>
                 <textarea placeholder="Request Details" class="full-width"></textarea>
+             
 
                 <label class="consent">
                     <input type="checkbox"> I consent to have this website store my submitted information.
                 </label>
 
-                <button type="submit" class="btn">Get Started</button>
+                <button type="button" class="btn btn-primary">SUBMIT</button>
             </form>
         </div>
     </section>
