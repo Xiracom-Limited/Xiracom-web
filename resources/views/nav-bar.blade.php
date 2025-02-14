a02ec914da5eb99957abcc6c4289bb209421387c
Proffesuer <levischisira0@gmail.com>
2025-02-14 11:57:40 +0300

    solved a conflict

---
+++ resources/views/nav-bar.blade.php
@@ -0,0 +1,157 @@
+
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  @vite(['resources/sass/app.scss','resources/js/app.js'])
+  
+  
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<style>
+  .nav-image{
+    margin-left:4%;
+    margin-right:4%
+}
+ 
+  @media screen and (max-width:600px) and (max-width:720px){
+
+    .nav-image{
+
+width: 33%;
+
+}
+
+  }
+ .color-text{
+
+color:#ffff !important;
+font-weight:bolder;
+
+
+ }
+ .color-text:hover{
+
+color:#399C23 !important;
+font-weight:bolder;
+
+
+
+ }
+
+ .contact-nav-bar{
+font-weight:bolder;
+
+
+
+
+ }
+.color{
+    background-color: rgba(0, 0, 0, 0.3); 
+  
+}
+@media screen and (max-width:600px) and (max-width:720px){
+    .color{
+    background-color:#ffff; 
+    
+  
+}
+.color-text{
+
+color:#12087E!important;
+font-weight:bolder;
+
+
+ }
+}
+
+</style>
+</head>
+<body>
+
+<nav class="navbar w-100 navbar-expand-lg navbar-light color custom-nav" style="position:fixed;z-index:2000;">
+  <button class="navbar-toggler ml-2" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon "></span>
+  </button>
+  <img src="images/xiracomlogo.png" width="10%" class="nav-image" style="">
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link fw-bolder color-text" href="./">HOME<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle fw-bolder color-text" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          ABOUT US
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item"  href="./about-us">Who we are </a>
+      <a class="dropdown-item"  href="./our-team">Our Team</a>
+      <a class="dropdown-item"  href="./jobs&internship">Jobs & Internships</a>
+      <a class="dropdown-item"  href="./our-projects">Our Projects</a>
+      <a class="dropdown-item"  href="./ourclients&testimonials">Our Clients</a>
+      <a class="dropdown-item"  href="./blog">Blog</a>
+      <a class="dropdown-item"  href="./contact-us">Contact Us</a>
+      <a class="dropdown-item"  href="./request-quote">Request Quote</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle fw-bolder color-text" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          SOFTWARE DEV
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="./uiuxdesign">UX/UI Design</a>
+          <a class="dropdown-item" href="./app-development">App Development</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">API Intergrations</a>
+          <a class="dropdown-item" href="./Hardware-software">H&S Maintenance </a>
+          <a class="dropdown-item" href="./domain-hosting-services">Domain & Hosting</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle color-text fw-bolder" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          OUR BRANDS
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="./Darasalink">DarasaLink</a>
+          <a class="dropdown-item" href="./Examparlour">Examparlour</a>
+          <a class="dropdown-item" href="./bulk-sms">Onfod Bulk SMS</a>
+          <a class="dropdown-item"  href="https://darasalink.co.ke/shop/">Xi Shop</a>
+          <a class="dropdown-item" href="#">Xiracom BTC</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle color-text fw-bolder" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+       OTHER SERVICES
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item"  href="./consultancy">Tech Consultancy</a>
+      <a class="dropdown-item"  href="./skiza-tunes&SSID-codes">Skiza Tunes</a>
+      <a class="dropdown-item"  href="./cyber-security">Cybersecurity</a>
+      <a class="dropdown-item"  href="./Digital-marketing">Digital Marketing</a>
+      <a class="dropdown-item"  href="./training&bootcamps">Trainings & Bootcamps</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link fw-bolder color-text" href="./login">LOGIN</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link fw-bolder color-text" href="./register">GET STARTED</a>
+      </li>
+    </ul>
+  <div class="me-5 contact-nav-bar" style="color:#ffff;">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+</svg>
+    
+      +254704907555
+
+  </div>
+  </div>
+</nav>
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  
+</body>
