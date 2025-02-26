a1ab8342f56e9baff4350c7596c53c35fb2fab2e
Proffesuer <levischisira0@gmail.com>
2025-02-26 20:22:36 +0300

    added content

--- resources/views/Hardware-software.blade.php
+++ resources/views/Hardware-software.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hardware Software Maintenance</title>
+    <title>Hardware & Software Maintenance - Xiracom Limited</title>
     <link rel="stylesheet" href="style/hardware-software.css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
     @vite(['resources/sass/app.scss','resources/js/app.js'])
